deafa7f192bd61eccda168114a6e6f811e6a3925
ayushkumar912 <kumarayush912@icloud.com>
2024-04-12 17:56:44 +0530

    Add .gitignore to ignore node_modules directory

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Campground = require('./models/campground');
+const Campground = require("./models/campground");
 const path = require("path");
+const methodOverride = require("method-override");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp");
+mongoose.connect("mongodb://localhost:27017/yelp-camp"); //Database connection made using this
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+db.on("error", console.error.bind(console, "connection error:")); //verify that weather database is connected successfully or not
 db.once("open", () => {
   console.log("DataBase Connected");
 });
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -19,13 +24,47 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds =await Campground.find({});
-    console.log(campgrounds);
-    res.render('campgrounds/index', { campgrounds });
-
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  // console.log(campgrounds);
+  res.render("campgrounds/index", { campgrounds });
 });
-  
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  // const campground = new Campground.
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  // console.log(campgrounds);
+  res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect(`/campgrounds`);
+});
 
 // app.get('/makecampground', async(req,res) =>{
 //    const camp =new Campground({title:'My Backyard', description:'cheap camping'});
