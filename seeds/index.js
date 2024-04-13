02c2b41d4a18bf83feb01a25a0cc5191b9d3078c
ayushkumar912 <kumarayush912@icloud.com>
2024-04-13 17:27:18 +0530

    basic style using bootstrap for working

--- seeds/index.js
+++ seeds/index.js
@@ -20,10 +20,14 @@ const seedDB = async () => {
 
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price =Math.floor(Math.random()*100)+10;
     const camp = new Campground({
-      location: `${cities[random1000].city},${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
-    });
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: "In a fast-paced world dominated by technology and constant connectivity, the timeless allure of camping stands as a sanctuary of simplicity and serenity. Camping provides a unique opportunity to reconnect with nature, immersing oneself in the beauty of pristine landscapes and the soothing sounds of the wilderness. Surrounded by towering trees, expansive skies, and the gentle embrace of the elements, campers find solace in the simplicity of nature's embrace. Moreover, camping offers a welcome respite from the digital noise that pervades modern life. By unplugging from screens and devices, campers are able to rediscover the joy of genuine human connection, engaging in meaningful conversations and shared experiences under the open sky.",
+      price
+    });    
     await camp.save();
   }
 };
